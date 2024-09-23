bc24581e0df83c7585ad666f03b27eeb9c134d9c
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-24 01:54:06 +0530

    Get the relevant data from the api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Card from "./components/Card.jsx";
 
 function App() {
   return (
     <>
       <h2>Pokemon Avatar</h2>
+      <Card />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Card() {
-  return <div>Name</div>;
+  const [pokemon, setPokemon] = useState();
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/55")
+      .then((response) => {
+        console.log(response.data);
+
+        setPokemon(response.data);
+      })
+      .catch((err) => console.error("ERROR while fetching data", err));
+  }, []);
+  return (
+    <div>
+      <img
+        src={pokemon?.sprites.other.dream_world.front_default}
+        alt="pokemon-avatar"
+      />
+      <p>Name: {pokemon?.name}</p>
+      <div>
+        Abilities:
+        {pokemon.abilities.map((value, index) => (
+          <div key={index}> {value.ability.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
