45264cae84e95a4c3916e2b05df6c4e94a5bd450
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-26 17:34:25 +0530

    Push code to GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,93 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Card from "./components/Card.jsx";
+import axios from "axios";
 
 function App() {
+  const inputRef = useRef();
+  const [pokeData, setPokeData] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const nextData = () => {
+    setCount((c) => c + 20);
+  };
+
+  const prevData = () => {
+    setCount((c) => c - 20);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=${count}&limit=20`)
+      .then((response) => {
+        setPokeData(response.data.results);
+      })
+      .catch((err) => {
+        console.log("ERROR while getting Pokemon Data ", err);
+      });
+  }, [count]);
+
+  const searchPokemon = () => {
+    const inputValue = inputRef.current.value.toLowerCase();
+    if (!inputValue) {
+      return;
+    }
+    setPokeData([{ name: inputValue }]);
+    inputRef.current.value = "";
+  };
+
   return (
-    <div className=" bg-[#202829] h-screen">
-      <div className=" pt-12 text-gray-300">
+    <div className={` bg-[#202829] ${pokeData.length === 1 ? "h-screen" : ""}`}>
+      <div className=" pt-12 text-gray-300 px-4">
         <h2 className=" text-center mb-4 text-4xl font-bold">Pokemon Avatar</h2>
-        <Card />
+        <div className=" absolute top-2 right-2 md:top-12 md:right-16">
+          <button
+            onClick={prevData}
+            disabled={count === 0 ? true : false}
+            className={` px-2 py-1 bg-blue-700 text-gray-300 mx-2 rounded-lg ${
+              count === 0 ? "opacity-0" : ""
+            }`}
+          >
+            Prev
+          </button>
+          <button
+            onClick={nextData}
+            className=" px-2 py-1 bg-blue-700 text-gray-300 mx-2 rounded-lg"
+          >
+            Next
+          </button>
+        </div>
+        <div className=" w-full px-4 flex flex-wrap justify-center items-center">
+          <input
+            ref={inputRef}
+            className="w-[70%] h-10 pl-2 rounded-l-lg text-[#202829] outline-none"
+            type="text"
+            placeholder="Search Pokemon..."
+          />
+          <button
+            onClick={searchPokemon}
+            className=" px-4 py-2 rounded-r-lg my-4 bg-blue-700 text-white"
+          >
+            Search
+          </button>
+        </div>
+        <div
+          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 pb-4 `}
+        >
+          {pokeData.length === 0 ? (
+            <div className=" text-red-700 font-bold text-xl">
+              No Pokemon data available
+            </div>
+          ) : (
+            pokeData.map((item, index) => (
+              <div
+                key={index}
+                className={pokeData.length === 1 ? "col-span-4" : ""}
+              >
+                <Card name={item.name} />
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,89 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function Card() {
-  const inputRef = useRef();
+function Card({ name }) {
   const [pokemon, setPokemon] = useState();
-  const [num, setNum] = useState(1);
-
-  const nextPokemon = () => {
-    setNum(num + 1);
-  };
-  const prevPokemon = () => {
-    setNum(num - 1);
-  };
 
   useEffect(() => {
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${num}/`)
-      // .get(`https://pokeapi.co/api/v2/pokemon/pikachu/`)
+      .get(`https://pokeapi.co/api/v2/pokemon/${name}/`)
       .then((response) => {
-        console.log(response.data);
         setPokemon(response.data);
       })
       .catch((err) => console.error("ERROR while fetching data", err));
-  }, [num]);
-
-  const findPokemon = () => {
-    const inputValue = inputRef.current.value.toLowerCase();
-    console.log(inputValue);
-
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
-      .then((response) => {
-        setNum(response.data.id);
-      })
-      .catch((err) => console.error("ERROR while getting search data", err));
-
-    inputRef.current.value = "";
-  };
+  }, [name]);
 
   return (
-    <div className=" w-[90%] lg:w-[30%] mx-auto shadow-xl rounded-lg shadow-[#363634] pb-2">
-      <div className=" w-full px-4 flex flex-wrap items-center">
-        <input
-          ref={inputRef}
-          className="w-[70%] h-10 pl-2 rounded-l-lg text-[#202829] outline-none"
-          type="text"
-          placeholder="Search Pokemon..."
-        />
-        <button
-          onClick={findPokemon}
-          className=" px-4 py-2 rounded-r-lg my-4 bg-blue-700 text-white"
-        >
-          Search
-        </button>
-      </div>
+    <div className=" w-full mx-auto shadow-xl rounded-lg shadow-[#363634] pb-2">
       <img
-        className=" w-[90%] mx-auto h-[250px]"
+        className=" w-full mx-auto h-[250px]"
         src={pokemon?.sprites.other.dream_world.front_default}
         alt="pokemon-avatar"
       />
       <p className=" text-center my-4 font-semibold text-2xl">
         Name: {pokemon?.name}
       </p>
-      <div className=" flex justify-center gap-2 font-semibold text-xl">
+      <div className=" flex justify-center gap-2 font-semibold text-sm">
         Abilities:
         {pokemon?.abilities.map((value, index) => (
           <div key={index}> {value.ability.name}</div>
         ))}
-      </div>
-      <div className=" flex justify-between items-center">
-        <button
-          onClick={prevPokemon}
-          disabled={num === 1}
-          className={`w-36 px-4 py-2 rounded-lg my-4 bg-blue-700 text-white  mx-auto ${
-            num === 1 ? "opacity-10" : ""
-          }`}
-        >
-          Previous
-        </button>
-        <button
-          onClick={nextPokemon}
-          className=" w-36 px-4 py-2 rounded-lg my-4 bg-blue-700 text-white  mx-auto"
-        >
-          Next
-        </button>
       </div>
     </div>
   );
